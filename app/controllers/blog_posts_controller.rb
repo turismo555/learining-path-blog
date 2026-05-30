d1c815f83ca008ffae1eef94c04432b8935485cc
turismo555 <hailrake15@gmail.com>
2026-05-30 23:57:36 +0200

    Add published_at for draft, published, and scheduled blog posts

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,11 +2,11 @@ class BlogPostsController < ApplicationController
   before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @blog_posts = BlogPost.all
+    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
+    
   end 
 
   def new
@@ -40,12 +40,12 @@ class BlogPostsController < ApplicationController
 
   private
   def blog_post_params
-    params.require(:blog_post).permit(:title, :body)
+    params.require(:blog_post).permit(:title, :body, :published_at)
   end
 
   def set_blog_post
-    @blog_post = BlogPost.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
+    @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
     redirect_to root_path
   end
 
